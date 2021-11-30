f0221da2fed32e5b1624f824bfff5644826c3d7d
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2021-11-30 12:19:02 +0000

    build endpoint get /api/topics with happy path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,28 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+  test("status 200: responds with object containing all 3 topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((res) => {
+        expect(Array.isArray(res.body.topics)).toBe(true);
+        expect(res.body.topics).toHaveLength(3);
+        res.body.topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
