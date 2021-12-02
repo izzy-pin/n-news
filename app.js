ad00bd909436d81adf02baafb6742a7b73386559
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2021-12-02 11:59:35 +0000

    add msg obj for get '/'

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const apiRouter = require("./routes/api.router");
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res, next) => {
+  res.status(200).send({ msg: "Hello from the news api!" });
+});
+
 app.use("/api", apiRouter);
 app.all("/*", invalidPathErrorHandler);
 app.use(handleCustomErrors);
